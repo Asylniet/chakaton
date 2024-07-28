7815818aad082f615413cb827214ff74c108f390
Rahugg <rahugg07@gmail.com>
2024-07-28 17:31:20 +0500

    add content

--- src/lib/constants.tsx
+++ src/lib/constants.tsx
@@ -26,41 +26,94 @@ export const locations: TLocation[] = [
             '/al-haram-2.jpg',
             '/al-haram-3.jpeg',
         ],
-        description: "Good place to visit",
-        overview: "SOME OVERVIEW zhe est zhe",
+        description: "Мечеть Аль-Харам",
+        overview: <div className="mr-3">
+            <p className="mb-2">
+                Мечеть Аль-Харам - Первая мечеть на земле. Первый храм Единому Богу, установленный Адамом и
+                восстановленный после потопа Ибрахимом и Исмаилом, мир им всем.
+            </p>
+            <p className="mb-2"> Она является центром расположения
+                великой мусульманской святыни Каабы. Масджид аль-Харам, упоминаемая еще в коранических аятах,
+                является
+                киблой (направлением на святыню ислама во время совершаемой верующим молитвы) и местом обязательного
+                хаджа (паломничества), предписанного Кораном для каждого правоверного мусульманина. </p>
+            <p className="mb-2">Значимости ей
+                придает и тот факт, что здесь совершали намаз такие пророки Аллаха, как Адам, Ибрагим, Исмаил и
+                Мухаммад
+                (мир ему и благословение Всевышнего!).</p>
+            <p className="mb-2">
+                <span
+                    className="font-bold">
+                Но почему же "запретная"?</span> С арабского "харам" означает "запретный", "неприкосновенный",
+                "святыня",
+                "священное место". На территории мечети Харам, почитаемой священной, изначально было строжайше запрещено
+                убийство людей и животных, ведение боев, а также ее посещение не мусульманами (иноверцами).
+            </p>
+            <p className="mb-2">
+                О достоинстве совершения намаза в трех главных святынях (мечетях) Пророк Мухаммад, говорил: «Чтение
+                намаза в мечети аль-Харам подобно чтению 100 тысяч намазов в обычной мечети. Чтение намаза в моей мечети
+                (в мечети Пророка в Медине) равно чтению 1 тысячи намазов. А чтение намаза в Бейтуль-Макдис (мечети
+                аль-Акса) приравнивается к 500 намазам».
+            </p>
+        </div>,
         amal: <>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid animi,{' '}
-            <ContextLink content={<>
-                <ContextLink content='some cool contextual text' depth={2}> LETS LINK IT </ContextLink>
-            </>}>
-                Anim amet sit in
+            <ContextLink content={
+                <>
+                    <div className="flex gap-2 bg-[#f4f4f5] p-[5px] rounded-[15px]">
+                        <div>
+                            <p>Действия при входе в мечеть Аль-Харам</p>
+                            <p className="ml-6 mt-2 text-[20px]">
+                                <p className="mb-2">
+                                    1)то есть даже не снимая груз с транспорта, по прибытии в гостиницу или на стоянку,
+                                    оставляя все как есть, поручив одному смотреть за вещами нужно отправиться в
+                                    Масжидуль
+                                    Харам на тавафул куддум (таваф прибытия в Мекку). Не надо даже переодеваться. Не
+                                    надо
+                                    даже заносить вещи в номера. Это желательно так делать. Если слишком устали или
+                                    мешают
+                                    другие, можно и в другое время совершить таваф. Тавафул куддум можно совершить в
+                                    любое
+                                    время до отправления на Арафат.
+                                </p>
+                                <p className="mb-2">
+                                    3) покаяться и чуть задержаться в мыслях, что очищаешься от грехов. Вспоминают
+                                    достоинство священной мечети и том, что это самый ценный дом Аллаха
+                                </p>
+                                <p className="mb-2">
+                                    6) Перевод: "Я прошу защиты Великим Аллахом и Его прекрасной сущностью, Его
+                                    изначальным
+                                    царством, от сатаны, изгнанного от Его милости. С помощью имени Аллаха я вхожу, и
+                                    вся
+                                    хвала Аллаху.Нет силы и мощи оставить плохое и совершать поклонение, кроме как с
+                                    помощью
+                                    Всевышнего Аллаха. (Затем читают салават Пророку (мир ему и благословение Аллаха)).
+                                    Затем произносят: "О, мой Аллах! Ты смой грехи мои и открой мне врата Своей
+                                    милости".
+                                    При выходе из мечети читают это же дуа, только лишь там, где читают "ли абваба
+                                    рахматика", нужно читать "абваба ризкика". Это дуа желательно читать при входе и
+                                    выходе
+                                    не только в Масжидуль Харам, но и во всех других мечетях. "Умдат", с. 39.
+                                </p>
+                            </p>
+                        </div>
+                    </div>
+                </>
+            } triggerClassname='text-[16px] font-bold text-[#09090B] no-underline'>
+                <div>
+                    <img className="rounded-[15px] w-[90%] mx-auto" src='/al-haram-how-to.webp'/>
+                    <ul className="ml-6 mt-2 text-[20px]">
+                        <li>1) (По возможности) Сразу же идти на тавафул Масжидуль Харам на тавафул куддум</li>
+                        <li>2) (По возможности) Входят в мечеть через ворота "Бабу ссалам"
+                        </li>
+                        <li>3) При приближении к воротам мечети нужно заново покаяться
+                        </li>
+                        <li>4) Всегда входят с правой ноги, выходят с левой (в любой мечети)
+                        </li>
+                        <li>5) При входе и выходе внутрь читают дуа
+                        </li>
+                    </ul>
+                </div>
             </ContextLink>
-            Qui proident magna commodo enim sint dolor eiusmod. Nisi eiusmod nisi officia
-            reprehenderit eu minim labore non incididunt nostrud enim sint sit non.
-            esse consequat commodo voluptate tempor. Qui ea sunt officia fugiat pariatur amet
-            commodo nostrud laboris est incididunt qui ad. Quis ea adipisicing esse commodo pariatur
-            esse reprehenderit anim Lorem magna consequat. Qui minim reprehenderit exercitation
-            excepteur commodo dolore do ut.
-
-            Cupidatat sit excepteur cillum tempor esse. Ut anim dolor aute commodo irure ipsum enim
-            anim veniam et ad est ipsum. Quis minim ullamco occaecat eu aute ullamco duis
-            consectetur velit proident. Magna labore cupidatat qui enim labore laboris incididunt.
-            Duis nostrud fugiat nulla eu anim voluptate duis pariatur voluptate magna amet cupidatat
-            laboris.
-
-            Irure mollit consequat qui eiusmod aliqua eu voluptate enim ea et voluptate in
-            sit elit reprehenderit. Quis velit esse eiusmod ut consectetur aliqua Lorem eu
-            incididunt dolore consequat elit tempor adipisicing. Labore dolore ullamco mollit
-            deserunt consequat eiusmod sit deserunt eu deserunt mollit reprehenderit.
-
-            Nisi proident nostrud incididunt eu non cillum culpa. Qui laboris laboris irure sit
-            Lorem velit tempor amet incididunt voluptate ea esse do culpa sunt. Eu ad ea cillum eu
-            ut laboris ad qui excepteur enim ut enim cillum sit dolor. Tempor enim Lorem elit mollit
-            Lorem. Ea commodo aliqua nostrud anim veniam labore cupidatat sunt fugiat cillum
-            pariatur cillum cupidatat anim. Amet consequat velit est velit do ut nostrud aute anim
-            ipsum minim laborum laborum cillum aute. Ut officia incididunt velit. esse illo
-            itaque iusto, necessitatibus nulla officia omnis optio porro quaerat quibusdam quos
-            recusandae rem sit vero voluptatem. Accusantium, aliquid.
         </>,
         sira: "2",
     },
@@ -74,7 +127,71 @@ export const locations: TLocation[] = [
             '/maqam-2.jpg',
             '/maqam-3.webp',
         ],
-        overview: "Макам Ибрахима (место Ибрахима) — место в пределах комплекса Мечеть аль-Харам, на котором стоял пророк Ибрахим во время строительства Каабы. В настоящее время на этом месте находится небольшая постройка с куполом. В Коране макам Ибрахима упоминается дважды. Всевышний говорит (смысл): И мы приказали людям сделать макам Ибрахима (мир ему) местом для молитвы (Коран: 2/185). Макам - это место стояния. Местом стояния Ибрахима (мир ему) является камень, огороженный стеклянным столбом. При строительстве Каабы Ибрахим (мир ему) стоял на нем. Когда стена поднялась, и камень поднимался, т.е. Всевышний покорил Ибрахиму (мир ему) этот камень. Пророк Исмаил (мир ему) подавал камни пророку Ибрахиму (мир ему) и тот камень, на котором он стоял, поднимался, опускался или обходил по необходимости Каабу на подобии подъемного крана. Этот макам (камень) пришел из Рая вместе с пророком Адамом В хадисе сказано: Черный камень и макам являются яхонтами из яхонтов Рая. Если бы Всевышний не погасил их сияние, то они бы осветили все, между востоком и западом (Тирмизи, Хаким). Этот макам является одним из чудес (муъжизат) пророка Ибрахима (мир ему). Всевышний сделал камень мягким для него. До сих пор на нем видны следы обеих ног Ибрахима (мир ему).",
+        description: "место Ибрахима",
+        overview: <div className="mr-3">
+            <p className="mb-2">Макам Ибрахима (место Ибрахима) — место в пределах комплекса Мечеть аль-Харам, на
+                котором стоял пророк Ибрахим во время строительства Каабы. В настоящее время на этом месте находится
+                небольшая постройка с куполом.</p>
+            <p className="mb-2">Этот макам является одним из чудес (муъжизат) пророка Ибрахима (мир ему). Всевышний
+                сделал камень мягким для него. До сих пор на нем видны следы обеих ног Ибрахима (мир ему).
+            </p>
+            <p className="mb-2">
+                При строительстве Каабы Ибрахим (мир ему) стоял на нем. Когда стена поднялась, и камень поднимался, т.е.
+                Всевышний покорил Ибрахиму (мир ему) этот камень. Пророк Исмаил (мир ему) подавал камни пророку Ибрахиму
+                (мир ему) и тот камень, на котором он стоял, поднимался, опускался или обходил по необходимости Каабу на
+                подобии подъемного крана. Этот макам (камень) пришел из Рая вместе с пророком Адамом
+            </p>
+            <p className="mb-2">
+                В хадисе сказано: Черный камень и макам являются яхонтами из яхонтов Рая. Если бы Всевышний не погасил
+                их сияние, то они бы осветили все, между востоком и западом (Тирмизи, Хаким).
+            </p>
+            <p className="mb-2">
+                Intersting facts!
+                В Коране макам Ибрахима упоминается дважды. Всевышний говорит (смысл):
+                И мы приказали людям сделать макам Ибрахима (мир ему) местом для молитвы (Коран: 2/185).
+            </p>
+        </div>,
+        amal: <>
+            <ContextLink content={
+                <>
+                    <div className="flex gap-2 bg-[#f4f4f5] p-[5px] rounded-[15px]">
+                        <div>
+                            <p>Чтение два ракаат намаза за макамом</p>
+                            <p className="ml-6 mt-2 text-[20px]">
+                                Сподвижники Пророка (с.а.в.) и
+                                его праведные последователи привыкли собираться у макама Ибрахима для совершения намаза,
+                                поминания Аллаха, дуа и других видов поклонения. При наличии возможности совершение
+                                намаза-таваф в макаме Ибрахима является рекомендуемым (мустахаб) действием.
+                                <div className="flex gap-2 bg-[#f4f4f51] p-[5px] rounded-[15px]">
+                                    <div>
+                                        <p>Совершение намаза за макамом не должно быть именно за макамом</p>
+                                        <p className="text-[14px] text-[#71717A] font-regular mt-2">Когда ты пьешь воду
+                                            Можно совершить на
+                                            той
+                                            стороне, где будет удобно. Совершение на любом месте сзади макама Ибрахима
+                                            (мир ему)
+                                            намаза-суннат исполняется. Также исполняется суннат стояния за
+                                            Макамом.(Аль-хаджу
+                                            фазаилул ва ахIкамун, с. 259-262).
+                                            Поэтому после двух ракаатов тавафа здесь нужно постараться от души прочитать
+                                            дуа
+                                            потому что это место где принимаются мольбы</p>
+                                    </div>
+                                </div>
+                            </p>
+                        </div>
+                    </div>
+                </>
+            } triggerClassname='text-[16px] font-bold text-[#09090B] no-underline'>
+                <div>
+                    <img className="rounded-[15px] w-[90%] mx-auto" src='/maqam-how-to.jpg'/>
+                    <ul className="ml-6 mt-2 text-[20px]">
+                        <li>1) Чтение два ракаат намаза за макамом</li>
+
+                    </ul>
+                </div>
+            </ContextLink>
+        </>,
         sira: "3",
     },
     {
@@ -85,11 +202,17 @@ export const locations: TLocation[] = [
         images: [
             '/zamzam-1.jpg',
         ],
-        overview: <div className="mr-3"><p className="mb-2">В самой главной мечети мусульман в Мекке в 18 метрах от Чёрного камня находится священный колодец Зам-зам. История его возникновения связана с жизнью двух пророков - Ибрахима и его сына Исмаила (мир ему)</p> 
-        <p className="mb-2">В настоящее время в мечети Аль-Харам есть более чем 700 кранов, ежедневно сотни тысяч паломников берут оттуда воду Замзам. В добавок к этому тысячи специальных машин развозят воду Замзам по городам Саудии. И хвала Всевышнему, не смотря на это вода источника Замзам не кончается. <span className="font-bold">Разве это не является доказательством величия Всевышнего Аллаха?!</span></p></div>,
+        overview: <div className="mr-3"><p className="mb-2">В самой главной мечети мусульман в Мекке в 18 метрах от
+            Чёрного камня находится священный колодец Зам-зам. История его возникновения связана с жизнью двух пророков
+            - Ибрахима и его сына Исмаила (мир ему)</p>
+            <p className="mb-2">В настоящее время в мечети Аль-Харам есть более чем 700 кранов, ежедневно сотни тысяч
+                паломников берут оттуда воду Замзам. В добавок к этому тысячи специальных машин развозят воду Замзам по
+                городам Саудии. И хвала Всевышнему, не смотря на это вода источника Замзам не кончается. <span
+                    className="font-bold">Разве это не является доказательством величия Всевышнего Аллаха?!</span></p>
+        </div>,
         amal:
-        <>
-            <ContextLink content={<>
+            <>
+                <ContextLink content={<>
                     <div>
                         <img className="rounded-[15px] w-[90%] mx-auto" src='/zamzam.jpeg'/>
                         <p className="text-black text-center text-[24px] font-bold mt-2">Правила питья воды Замзам</p>
@@ -101,16 +224,17 @@ export const locations: TLocation[] = [
                         </ul>
                     </div>
                 </>} triggerClassname='text-[16px] font-bold text-[#09090B] no-underline'>
-                <div className="flex gap-2 bg-[#f4f4f5] p-[5px] rounded-[15px]">
-                    <img className="w-[125px] h-[125px] rounded-[10px]" src={"/zamzam.jpeg"}/>
-                    <div>
-                        <p>Правила питья воды Замзам</p>
-                        <p className="text-[14px] text-[#71717A] font-regular mt-2">Когда ты пьешь воду Зам-зам, ты должен обратиться в сторону киблы...</p>
+                    <div className="flex gap-2 bg-[#f4f4f5] p-[5px] rounded-[15px]">
+                        <img className="w-[125px] h-[125px] rounded-[10px]" src={"/zamzam.jpeg"}/>
+                        <div>
+                            <p>Правила питья воды Замзам</p>
+                            <p className="text-[14px] text-[#71717A] font-regular mt-2">Когда ты пьешь воду Зам-зам, ты
+                                должен обратиться в сторону киблы...</p>
+                        </div>
                     </div>
-                </div>
-                
-            </ContextLink>
-        </>,
+
+                </ContextLink>
+            </>,
         sira: "3",
     },
 ];
